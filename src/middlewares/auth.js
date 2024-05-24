ee25f99147f13bcc4216e28129e042af5927ee40
Smart-Machine <radu.calin500@gmail.com>
2024-05-24 07:08:42 +0300

    feauture: add middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const token = req.header('Authorization').replace('Bearer ', '');
+  if (!token) {
+    return res.status(401).json({ message: 'Access Denied' });
+  }
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).json({ message: 'Invalid Token' });
+  }
+};
+
+const authorizeRole = (role) => {
+  return (req, res, next) => {
+    if (req.user.role !== role) {
+      return res.status(403).json({ message: 'Access Forbidden' });
+    }
+    next();
+  };
+};
+
+module.exports = { verifyToken, authorizeRole };
+
